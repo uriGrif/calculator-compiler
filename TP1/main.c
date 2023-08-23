58418b1d58c20225d3fa0c69254cb8fc81bc997d
Gonzalo Ivo Turri <gturri@frba.utn.edu.ar>
2023-08-23 20:19:09 -0300

    hago las funciones auxiliares y enum de columnas + arranco scanner

--- TP1/main.c
+++ TP1/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
+#include<ctype.h>
 #include "scanner.h"
 
 int main(void) {
-    printf("Hello Scanner");
     return 0;
 }
--- TP1/scanner.c
+++ TP1/scanner.c
@@ -1,5 +1,8 @@
 #include <ctype.h>
+#include<stdio.h>
 #include "scanner.h"
+
+
 
 char lexema[200];
 
@@ -20,6 +23,20 @@ enum estados {
     ESCAPE
 };
 
+
+
+enum columnas {
+    CARACTER_CERO,
+    DIGITS_1_AL_9,
+    LETRAS_HEXA,
+    EQUIS,
+    LETRAS_NO_HEXA,
+    FDT,
+    ESPACIO,
+    OTROS
+};
+
+
 enum estados TABLA_TRANSICION[13][8] = {
     {ARRANCA_CERO, RECON_CONST, RECON_ID, RECON_ID, RECON_ID, FDT_OK, INICIAL, RECON_ERR_GRAL},
     {RECON_ID, RECON_ID, RECON_ID, RECON_ID, RECON_ID, ID_OK, ID_OK, ID_OK},
@@ -36,6 +53,31 @@ enum estados TABLA_TRANSICION[13][8] = {
     {ESCAPE, ESCAPE, ESCAPE, ESCAPE, ESCAPE, ESCAPE, ESCAPE, ESCAPE}
 };
 
+int aceptor(enum estados estado){
+    return estado >= 6 && estado<=9;
+}
+
+int centinela(enum estados estado){
+    return estado >=6 && estado <=8 || estado == 10 || estado == 12;
+}
+
+int debo_parar(enum estados estado){
+    return aceptor(estado) || centinela(estado);
+}
+
+enum columnas categoria_lexica(char caracter){
+    if(caracter == '0') return CARACTER_CERO;
+    if(isdigit(caracter)) return DIGITS_1_AL_9;
+    if(isxdigit(caracter)) return LETRAS_HEXA;
+}
+
 enum token scanner(void) {
+    enum estados estado = INICIAL;
+    char caracter;
+    while(!debo_parar(estado)){
+       caracter =  getchar();
+       estado = TABLA_TRANSICION[estado][categoria_lexica(caracter)];
+    }
     return ERROR_GRAL;
 }
+
